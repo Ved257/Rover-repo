0e8e5030d7da39421bef1b5bf4038055570dd181
codexbvn <80110118+codexbvn@users.noreply.github.com>
2022-01-16 19:43:32 +0545

    cams_js_updated

--- camera_script.js
+++ camera_script.js
@@ -4,10 +4,120 @@ if (typeof (document) != "undefined") {
           video: {},
         })
         .then((stream) => {
-          document.getElementById("vid").srcObject = stream;
+          document.getElementById("vid0").srcObject = stream;
         });
-    const button=document.getElementById("mybtn");
-    const vid=document.getElementById("vid");
+    const button=document.getElementById("mybtn0");
+    const vid=document.getElementById("vid0");
+    button.onclick=function(){
+        if(vid.style.display!=="none"){
+            // vid.removeAttribute('autoplay');
+            vid.style.display="none";
+        }
+        else{
+            vid.style.display="block";
+        }
+    };
+
+}
+
+if (typeof (document) != "undefined") {
+    navigator.mediaDevices
+        .getUserMedia({
+          video: {},
+        })
+        .then((stream) => {
+          document.getElementById("vid1").srcObject = stream;
+        });
+    const button=document.getElementById("mybtn1");
+    const vid=document.getElementById("vid1");
+    button.onclick=function(){
+        if(vid.style.display!=="none"){
+            // vid.removeAttribute('autoplay');
+            vid.style.display="none";
+        }
+        else{
+            vid.style.display="block";
+        }
+    };
+
+}
+
+if (typeof (document) != "undefined") {
+    navigator.mediaDevices
+        .getUserMedia({
+          video: {},
+        })
+        .then((stream) => {
+          document.getElementById("vid2").srcObject = stream;
+        });
+    const button=document.getElementById("mybtn2");
+    const vid=document.getElementById("vid2");
+    button.onclick=function(){
+        if(vid.style.display!=="none"){
+            // vid.removeAttribute('autoplay');
+            vid.style.display="none";
+        }
+        else{
+            vid.style.display="block";
+        }
+    };
+
+}
+
+if (typeof (document) != "undefined") {
+    navigator.mediaDevices
+        .getUserMedia({
+          video: {},
+        })
+        .then((stream) => {
+          document.getElementById("vid3").srcObject = stream;
+        });
+    const button=document.getElementById("mybtn3");
+    const vid=document.getElementById("vid3");
+    button.onclick=function(){
+        if(vid.style.display!=="none"){
+            // vid.removeAttribute('autoplay');
+            vid.style.display="none";
+        }
+        else{
+            vid.style.display="block";
+        }
+    };
+
+}
+
+if (typeof (document) != "undefined") {
+    navigator.mediaDevices
+        .getUserMedia({
+          video: {},
+        })
+        .then((stream) => {
+          document.getElementById("vid4").srcObject = stream;
+        });
+    const button=document.getElementById("mybtn4");
+    const vid=document.getElementById("vid4");
+    button.onclick=function(){
+        if(vid.style.display!=="none"){
+            // vid.removeAttribute('autoplay');
+            vid.style.display="none";
+        }
+        else{
+            vid.style.display="block";
+        }
+    };
+
+}
+
+if (typeof (document) != "undefined") {
+    navigator.mediaDevices
+        .getUserMedia({
+          video: {},
+        })
+        .then((stream) => {
+          document.getElementById("vid5").srcObject = stream;
+        });
+    const button=document.getElementById("mybtn5");
+    const vid=document.getElementById("vid5");
     button.onclick=function(){
         if(vid.style.display!=="none"){
             // vid.removeAttribute('autoplay');
